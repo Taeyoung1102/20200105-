d6f26ceb121371f49c34555e779e95e7358991aa
Taeyoung1102 <43396049+Taeyoung1102@users.noreply.github.com>
2020-01-08 11:30:51 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const sections = document.querySelectorAll('section');
 const bubble = document.querySelector('.bubble');
 const gradients = [
-    "linear-gradient(to right, #8DC26F, #76b852);" ,
-    " linear-gradient(to right, #F2C94C, #F2994A);",
+    "linear-gradient(to right, #8DC26F, #76b852)" ,
+    " linear-gradient(to right, #F2C94C, #F2994A)",
     "linear-gradient(to right, #C4E0E5, #4CA1AF)" 
 ];
 
 const options = {
     threshold: 0.7
-}
+    //70% 쯤에서 activate
+};
 
 let observer = new IntersectionObserver(navCheck, options);
 
 function navCheck(entries){
  entries.forEach(entry => {
-     console.log(entry);
-     const className = entry.className;
-     console.log(className);
-     const activeAnc
+     const className = entry.target.className;
+     const activeAnchor = document.querySelector(`[data-page=${className}`)
+     const gradientIndex = entry.target.getAttribute('data-index');
+     const coords = activeAnchor.getBoundingClientRect();
+     const  directions = {
+         height: coords.height,
+         width: coords.width,
+         top: coords.top,
+         left: coords.left
+     };
+     if(entry.isIntersecting){
+         bubble.style.setProperty('left', `${directions.left}px`);
+         bubble.style.setProperty('top', `${directions.top}px`);
+         bubble.style.setProperty('width', `${directions.width}px`);
+         bubble.style.setProperty('height', `${directions.height}px`);
+
+         bubble.style.background = gradients[gradientIndex]
+     }
  });
 }
 sections.forEach(section => {
